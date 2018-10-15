267f685738f1020e3484d345b12490847b3e8103
GIL SU PARK <prosild@naver.com>
2018-10-15 21:59:51 +0900

    백준 덱 10866번 문제 해결

--- BOJ/src/stack/Deque10866.java
+++ BOJ/src/stack/Deque10866.java
@@ -1,41 +1,66 @@
 package stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Deque10866 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int index;
-		String[] deque = new String[0];
-		
+		Deque<Integer> deque = new LinkedList<>();
+
 		do {
 			index = scan.nextInt();
 		} while (index < 1 || index > 10000);
-		
-		String[] cmd = new String[index];
-		
-		for (int idx = 0; idx < cmd.length; idx++) {
-			
-			cmd[idx] = scan.nextLine();
-			
-		}
-		
-		for (int idx = 0; idx < cmd.length; idx++) {
-			
-			String[] temp = cmd[idx].split(" ");
-			
-			if (temp[0].equals("push_back")) {
+
+		for (int idx = 0; idx < index; idx++) {
+
+			String cmd = scan.next();
+
+			if (cmd.equals("push_front")) {
+
+				int value = scan.nextInt();
+
+				deque.addFirst(value);
+
+			} else if (cmd.equals("push_back")) {
+
+				int value = scan.nextInt();
+
+				deque.addLast(value);
+
+			} else if (cmd.equals("pop_front")) {
 				
-				String[] temps = new String[deque.length + 1];
-				temps = deque.clone();
+				System.out.println(deque.isEmpty() ? -1 : deque.pop());
+				
+			} else if (cmd.equals("pop_back")) {
+				
+				System.out.println(deque.isEmpty() ? -1 : deque.removeLast());
+				
+			} else if (cmd.equals("size")) {
+				
+				System.out.println(deque.size());
+				
+			} else if (cmd.equals("empty")) {
+				
+				System.out.println(deque.isEmpty() ? 1 : 0);
+				
+			} else if (cmd.equals("front")) {
+				
+				System.out.println(deque.isEmpty() ? -1 : deque.peek());
+				
+			} else if (cmd.equals("back")) {
+				
+				System.out.println(deque.isEmpty() ? -1 : deque.peekLast());
 				
 			}
-			
 		}
-		
+
 		scan.close();
 	}
+
 }
