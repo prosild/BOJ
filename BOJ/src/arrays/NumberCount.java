f7cdc7b0e672f7d53dde35dc15f6ee691c887f94
GIL SU PARK <prosild@naver.com>
2018-10-22 13:37:49 +0900

    백준 함수 셀프넘버 4673번 해결

---
+++ BOJ/src/arrays/NumberCount.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class NumberCount {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int mulResult = 1;
+		int result[] = new int[10];
+		
+		for (int idx = 0; idx < 3; idx++) {
+			mulResult *= scan.nextInt();
+		}
+		
+		String temp = String.valueOf(mulResult);
+		
+		System.out.println(temp.charAt(0));
+		
+		for (int idx = 0; idx < temp.length(); idx++) {
+			for (int idx2 = 0; idx2 < 10; idx2++) {
+				
+				if (temp.charAt(idx) == idx2) {
+					result[idx2]++;
+				}
+			}
+		}
+
+		for (int r : result) {
+			System.out.println(r);
+		}
+		
+		scan.close();
+	}
+}
---
+++ BOJ/src/function/SelfNumber.java
@@ -0,0 +1,48 @@
+package function;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> selfNumber = new ArrayList<>();
+
+		int number = 0;
+		
+		while (number < 10000) {
+			
+			number++;
+			
+			int n = selfNumberCheck(number);
+			
+			if (n < 10001) {
+				selfNumber.add(n);
+			}
+		}
+		
+		for (int idx = 1; idx < 10001; idx++) {
+			
+			if (!selfNumber.contains(idx)) {
+				System.out.println(idx);
+			}
+		}
+		
+		
+	}
+	
+	public static int selfNumberCheck(int number) {
+		
+		int sum = number;
+		
+		while (number > 0) {
+			
+			sum += number % 10;
+			number /= 10;
+			
+		}
+		
+		return sum;
+	}
+}
